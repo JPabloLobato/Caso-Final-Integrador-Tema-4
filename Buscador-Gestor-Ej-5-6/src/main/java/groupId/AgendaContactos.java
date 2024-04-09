3311d7fa0ec18ea9bb1d1cc354990aab27fa6438
JPabloLobato <146821982+JPabloLobato@users.noreply.github.com>
2024-04-09 22:32:10 +0200

    Commit AgendaContactos.java eliminada y creada de nuevo

--- Buscador-Gestor-Ej-5-6/src/main/java/groupId/AgendaContactos.java
+++ Buscador-Gestor-Ej-5-6/src/main/java/groupId/AgendaContactos.java
@@ -1,4 +1,20 @@
 package groupId;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class AgendaContactos {
+    private final List<Contacto> contactos;
+
+    public AgendaContactos() {
+        this.contactos = new ArrayList<>();
+    }
+
+    public void agregarContacto(Contacto contacto) {
+        this.contactos.add(contacto);
+    }
+
+    public List<Contacto> getContactos() {
+        return new ArrayList<>(this.contactos);
+    }
 }
